bbded3d1d8bd0078fbd04f356833f01aa82a2a53
nacho710 <ignbaena@ucm.es>
2020-02-20 18:13:09 +0100

    Primera version de la pagina web donde alojar el juego

--- game.js
+++ game.js
@@ -1,13 +1,22 @@
 export default class Game extends Phaser.Scene {
-  constructor() {
-    super({ key: 'main' });
-  }
-  preload() {  
-  }
+    constructor() {
+        super({ key: 'main' });
 
-  create() {
-  }
 
-  update(time, delta) {    
-  }
+
+    }
+
+
+    preload() {
+        this.load.image('sky', '/assets/Fondo-gris.png');
+
+    }
+    create() {
+        var platforms;
+        this.add.image(600, 300, 'sky');
+    }
+
+
+    update(time, delta) {}
+
 }
